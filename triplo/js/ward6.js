a402e6d25e68987185ccca5d2af5825c6c18a6ad
Tim Erickson <tim@politalk.org>
2017-01-27 16:46:15 -0600

    Initial Commit

---
+++ triplo/js/ward6.js
@@ -0,0 +1,24 @@
+
+(function ($) {
+  
+  // controls background image on menu items
+
+  $(document).ready(function() {
+    var mainMenu = document.querySelector("#main-menu").getElementsByTagName("li");
+    for (a = 0 ; a < mainMenu.length ; a++) {
+      var temp = mainMenu[a];
+      var checker = $(temp).hasClass("active");
+      if (checker != true) {
+        mainMenu[a].addEventListener('mouseenter', function(a) {
+          this.classList.toggle('selected');
+        });
+      }
+      if (checker != true) {
+        mainMenu[a].addEventListener('mouseleave', function(a) {
+          this.classList.toggle('selected');
+        });
+      }
+    } 
+  });
+
+}(jQuery));
